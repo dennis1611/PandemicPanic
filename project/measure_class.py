240c6ba19e12ae24d7a28bac80b6affb16b1fb52
Jelle Knibbe <J.J.Knibbe@student.tudelft.nl>
2020-09-14 16:19:43 +0200

    Added measure class, to be completed when region class is finished

---
+++ project/measure_class.py
@@ -0,0 +1,18 @@
+### This is a W.I.P., I'll improve it when Nigel finishes his class
+
+class measure:
+    def __init__(self, name, number, influence, type):
+        #
+        self.name = name
+        self.number = number
+        self.influence = influence
+        self.type = type
+
+    def __repr__(self,):
+        return f'''This measure has the following effects:
+        Reduce the R-value for the chosen regions with a factor {effect}
+        '''
+
+    def effect(self, R_old):
+        R_new = R_old * influence
+        return R_new
