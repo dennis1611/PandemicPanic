f6b00f393ead6748d015c5f49720e989757a33a0
Willem <W.S.Wolswijk@student.tudelft.nl>
2020-09-14 21:42:20 +0200

    Dichtheidsfactoren en leeftijdsfactoren af (verkijgbaar voor provincies en voor COROP-en).
    Bevolkingsaantallen zijn ook gevormd (per provincie of per COROP)
    
    Data bestanden zijn toegevoegd aan github

--- project/passive_factors.py
+++ project/passive_factors.py
@@ -1,16 +1,18 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from pandas import read_csv
 
-data = read_csv('data_prov.csv', skiprows=[0, 1, 2, 4], delimiter=";", index_col=0, skipinitialspace=True, decimal=',',
-                skipfooter=1, engine='python')
+# Provincie data verzamelen en in een DataFrame stoppen
+prov_data = read_csv('Data_provincies.csv', skiprows=[0, 1, 2, 4], delimiter=";",
+                     index_col=0, skipinitialspace=True, decimal=',', skipfooter=1, engine='python')
 
-names = list(data)
- # Unnamed kolommen weghalen
-for name in names:
-    if "Unnamed" in name:
-        data.pop(name)
+# COROP data verzamelen en in een DataFrame stoppen
+cor_data = read_csv('Data_coroppen.csv', skiprows=0, delimiter=";",
+                    index_col=1, skipinitialspace=True, decimal=',', skipfooter=1, engine='python')
+
+# Bepalen welke dataset je wilt gebruiken: de code is zo geschreven dat het allebei werkt
+data = cor_data
+
+# Eerste kolom weghalen; onnodige informatie
+data = data.iloc[:, 1:]
 
 # Rename columns
 columns = ['Bevolking', '0 tot 5 (%)', '5 tot 10 (%)', '10 tot 15 (%)', '15 tot 20 (%)', '20 tot 25 (%)',
@@ -20,24 +22,21 @@ data.columns = columns
 # Array van namen van regio's
 regios = data.index.values
 
-# Aparte DataFrame maken voor bevolking en dichtheid
+# Aparte DataFrame maken voor bevolkingsaantallen (handig voor het aantal besmettingen)
 Bevolkingsaantallen = data.filter(items=['Bevolking'])
-
-# Aparte DataFrame maken voor percentages
-percentages = data.filter(regex="(%)")
 
 
 # Functie om factoren voor de dichtheid te bepalen
-def dichtheidsfactoren(Algemene_dataset):
-    df = Algemene_dataset.filter(items=['Dichtheid'])
+def dichtheidsfactoren(algemene_dataset):
+    df = algemene_dataset.filter(items=['Dichtheid'])
 
-    # BELANGFACTOR: ALS DE DICHTHEID EEN TE GROTE/KLEINE ROL SPEELT IN DE SIMULATIE PAS JE DEZE AAN (hoger = meer invloed)
+    # BELANGFACTOR: ALS DE DICHTHEID EEN TE GROTE/KLEINE ROL SPEELT PAS JE DEZE AAN (hoger = meer invloed)
     belang = 0.4
 
     # Gemiddelde bepalen
     avg = float(df.mean(axis=0))
 
-    # Lijst van factoren aanmaken
+    # Lijst van factoren aanmaken (de data is als dict{} of als list[] verkrijgbaar, verander bij return)
     factors_dict = {}
     factors_list = []
 
@@ -51,6 +50,34 @@ def dichtheidsfactoren(Algemene_dataset):
         factors_dict[regios[i]] = factor
         factors_list.append(factor)
 
-    return factors_list
+    return factors_dict
 
-print(dichtheidsfactoren(data))
+
+# Functie om factoren voor de jeugd te bepalen
+def leeftijdsfactoren(algemene_dataset):
+    # 'Jeugd' DataFrame aanmaken beestaande uit kolommen van tussen de 15 en 25
+    jeugd = algemene_dataset.filter(items=['15 tot 20 (%)', '20 tot 25 (%)'])
+    jeugd["totale_jeugd (%)"] = jeugd['15 tot 20 (%)'] + jeugd['20 tot 25 (%)']
+
+    df = jeugd.filter(items=['totale_jeugd (%)'])
+
+    # ALS DE JEUGDVERHOUDING EEN TE GROTE/KLEINE ROL SPEELT PAS JE DEZE AAN (hoger = meer invloed)
+    belang = 2.0
+
+    # Gemiddelde bepalen
+    avg = float(df.mean(axis=0))
+
+    # Lijst van factoren aanmaken (de data is als dict{} of als list[] verkrijgbaar, verander bij return)
+    factors_dict = {}
+    factors_list = []
+
+    # Voor elk gebied de factor berekenen
+    for i in range(len(df)):
+        # Percentage jeugd vinden in DataFrame
+        procent_jeugd = df.iloc[i, 0]
+
+        # Factor berekenen en toevoegen
+        factor = (procent_jeugd / avg) ** belang
+        factors_dict[regios[i]] = factor
+        factors_list.append(factor)
+    return factors_dict
