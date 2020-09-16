0a1a13e13d26b752097360e292b78e6374bdeae0
Jelle Knibbe <J.J.Knibbe@student.tudelft.nl>
2020-09-16 12:02:44 +0200

    Updated measure class, added option for different types of effect, added method to print the description in a menu.

--- project/measure.py
+++ project/measure.py
@@ -1,18 +1,42 @@
-# This is a W.I.P., I'll improve it when Nigel finishes his class
-
 class Measure:
-    def __init__(self, name, number, influence, type):
-        #
+    """
+    Class that contains all aspects of a measure to be taken.
+    Give it a name, a number, a description for the player and determine the types and attributes.
+    Current types are:
+    > R_reduce: Reduce the R factor by R_influence
+    """
+    def __init__(self, name, number, desc, types, R_influence):
+        # there may be different types of measures with different effects
+        # maybe enter these as (a list of) strings?
         self.name = name
         self.number = number
-        self.influence = influence
-        self.type = type
+        self.desc = desc
+        self.types = types
+        if "R_reduce" in self.types:
+            self.R_influence = R_influence
 
-    def __repr__(self,):
-        return f'''This measure has the following effects:
-        Reduce the R-value for the chosen regions with a factor {self.effect}
-        '''
+    def __repr__(self):
+        string = f"This is measure number {self.number}, called: {self.name}, and it has the following effects:"
+        if "R_reduce" in self.types:
+            string += f"\nReduce the R factor by {self.R_influence}"
+        return string
 
     def effect(self, R_old):
-        R_new = R_old * self.influence
-        return R_new
+        """Apply the measure on the right stat, according to its type"""
+        if "R_reduce" in self.types:
+            R_new = R_old * self.R_influence
+            return R_new
+
+    def menu(self):
+        """"Print the description of the measure for the player"""
+        print(f"#{self.number}| {self.name}: {self.desc}")
+
+
+# # testing:
+# M1 = Measure("Half the R", 1, "It halves the R!", ["R_reduce"], 0.5)
+# R0 = 0.8
+# print(f"R0 is {R0}")
+# print(M1)
+# M1.menu()
+# R1 = M1.effect(R0)
+# print(f"R1 is {R1}")
