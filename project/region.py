6b09ab27fe5a772200ebbf6f6aedec846d9f16ed
Dennis <dennis16112000r@gmail.com>
2020-09-18 12:43:47 +0200

    adjust Region parameters

--- project/region.py
+++ project/region.py
@@ -10,11 +10,13 @@ class Region:
     Class that contains all information of a certain region.
     """
 
-    def __init__(self, name, inhabitants, infected=0):
+    def __init__(self, name, inhabitants, infectionfactor, deathfactor, infected=0, deaths=0):
         self.name = name
         self.inhabitants = inhabitants
+        self.infectionfactor = infectionfactor
+        self.deathfactor = deathfactor
         self.infected = infected
-        self.dead = 0
+        self.deaths = deaths
 
     def __repr__(self):
         string = f"This class is for {self.name} with a population of {self.inhabitants}"
