e1f6bc7d720c7deaa9d634a320d888f5e54a88b0
Jelle Knibbe <J.J.Knibbe@student.tudelft.nl>
2020-09-21 16:16:37 +0200

    Adjusted measure class by commenting out stuff that won't be used for the time being.

--- project/measure.py
+++ project/measure.py
@@ -6,7 +6,7 @@ class Measure:
     > R_reduce: Reduce the R factor by R_influence
     """
 
-    def __init__(self, name, number, desc, active, types, R_influence):
+    def __init__(self, name, number, desc, R_influence):
         # there may be different types of measures with different effects
         # maybe enter these as (a list of) strings?
         self.name = name
@@ -25,13 +25,6 @@ class Measure:
         if "R_reduce" in self.types:
             string += f"\nReduce the R factor by {self.R_influence}"
         return string
-
-    def effect(self, R_old):
-        """Apply the measure on the right stat, according to its type"""
-        if self.active:
-            if "R_reduce" in self.types:
-                R_new = R_old * self.R_influence
-                return R_new
 
     def menu(self):
         """"Print the description of the measure for the player"""
@@ -54,21 +47,20 @@ class Measure:
     especially during the use of buttons in the PyGame window.
     """
 
-    def activate(self):
-        self.active = True
-
-    def deactivate(self):
-        self.active = False
-
-    def is_active(self):
-        return self.active
-
-
-# # testing:
-# M1 = Measure("Half the R", 1, "It halves the R!", ["R_reduce"], 0.5)
-# R0 = 0.8
-# print(f"R0 is {R0}")
-# print(M1)
-# M1.menu()
-# R1 = M1.effect(R0)
-# print(f"R1 is {R1}")
+    #
+    # not needed for now
+    # def effect(self, R_old):
+    #     """Apply the measure on the right stat, according to its type"""
+    #     if self.active:
+    #         if "R_reduce" in self.types:
+    #             R_new = R_old * self.R_influence
+    #             return R_new
+    #
+    # def activate(self):
+    #     self.active = True
+    #
+    # def deactivate(self):
+    #     self.active = False
+    #
+    # def is_active(self):
+    #     return self.active
