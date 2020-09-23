dd9ad78ec30c531014310c387e1b20475653126f
Dennis <dennis16112000r@gmail.com>
2020-09-23 13:01:28 +0200

    fixed crash for no measure option

--- project/game.py
+++ project/game.py
@@ -1,7 +1,38 @@
 from pandas import read_csv
 from project.measure import Measure
 from project.region import Region
-import numpy as np
+
+
+# START initialising methods
+def initialise_measures():
+    """"Creates and returns a list of all measures"""
+    measures = []
+    numbers = []
+    with open("measures_data_simple.csv") as data:
+        next(data)  # skip first line
+        for line in data:
+            line = line.strip().split(",")
+            number = int(line[0])
+            name = line[1]
+            desc = line[2]
+            factor = float(line[3])
+            measures.append(Measure(number, name, desc, factor))
+            numbers.append(number)
+    return measures, numbers
+
+
+def initialise_regions():
+    """"Creates and returns a list of all regions"""
+    regions_df = read_csv('regional_data.csv', index_col=0)
+    region_instances = []
+    region_names = regions_df.index.values
+    for region in region_names:
+        region = Region(region, regions_df.loc[region, "Population"],
+                        regions_df.loc[region, "inf_factor"], regions_df.loc[region, "death_factor"])
+        region_instances.append(region)
+    return region_instances
+# END initialising methods
+
 
 # START weekly methods
 def update_infected(current_week: int):
@@ -46,59 +77,31 @@ def choose_measure():
     """"Displays all available measures to the user, and lets them choose one to take"""
     print('Choose one of the following measures:')
     for measure in measures:
-        measure.menu()  # Should probably be measure.menu() eventually
-    print('or "no measure" to take no action this turn')
+        measure.menu()
+    print('or #0| to take no action this turn')
 
     while True:
-        measure_chosen = input('Your choice: ') # This could probably use a better name
+        measure_chosen = input('Your choice (type a number): ')  # This could probably use a better name
         if int(measure_chosen) in measure_numbers:
             measure_taken = measures[int(measure_chosen)-1]
             print(f'You chose: {measure_taken.name}, but note that it is not used yet in this version')
-            break
-        elif measure_chosen == "no measure":
+            return measure_taken
+        elif int(measure_chosen) == 0:
             print('You decided to take no action, the game will move on')
-            break
+            return None
         else:
             print(f'Your input: {measure_chosen}, is not recognised, please try again')
-    return measure_taken
 
 
-def update_R(current_R, effect):
+def update_R(current_R, new_measure):
     """"Updates R based on the chosen measure"""
+    if isinstance(new_measure, Measure):
+        effect = new_measure.factor
+    else:
+        effect = 1
     new_R = current_R * effect
     return new_R
 # END weekly methods
-
-
-# START initialising methods
-def initialise_measures():
-    """"Creates and returns a list of all measures"""
-    measures = []
-    numbers = []
-    with open("measures_data_simple.csv") as data:
-        next(data) # skip first line
-        for line in data:
-            line = line.strip().split(",")
-            number = int(line[0])
-            name = line[1]
-            desc = line[2]
-            factor = float(line[3])
-            measures.append(Measure(number,name,desc,factor))
-            numbers.append(number)
-    return measures, numbers
-
-
-def initialise_regions():
-    """"Creates and returns a list of all regions"""
-    regions_df = read_csv('regional_data.csv', index_col=0)
-    region_instances = []
-    region_names = regions_df.index.values
-    for region in region_names:
-        region = Region(region, regions_df.loc[region, "Population"],
-                        regions_df.loc[region, "inf_factor"], regions_df.loc[region, "death_factor"])
-        region_instances.append(region)
-    return region_instances
-# END initialising methods
 
 
 # create general setup
@@ -108,6 +111,9 @@ R = [1.1]
 
 measures, measure_numbers = initialise_measures()
 regions = initialise_regions()
+
+# TODO: write an actual welcome message/introduction
+print('Welcome message/introduction')
 
 # main game loop
 week = 1
@@ -125,7 +131,7 @@ while running:
 
     measure_this_week = choose_measure()
 
-    R.append(update_R(R[-1], measure_this_week.factor))
+    R.append(update_R(R[-1], measure_this_week))
 
     # for measure in measure:
     #   measure.effect()
