778a1da5bb3ac9d4b07387ddc948fe9912db00c7
Dennis <dennis16112000r@gmail.com>
2020-09-28 20:36:34 +0200

    add option to rollback taken measures

--- project/game.py
+++ project/game.py
@@ -15,6 +15,33 @@ def display_report():
 
 def choose_measure():
     """"Displays all available measures to the user, and lets them choose one to take"""
+
+    def validate_measure_input(user_input):
+        """Returns a boolean whether the user's input to choose a measure is valid"""
+        is_int = True
+        number_chosen = -1
+        try:
+            number_chosen = int(user_input)
+        except ValueError:
+            is_int = False
+
+        # if: the input was an int
+        if is_int:
+            # case 1 (valid): the input was an int corresponding to a measure
+            if number_chosen in measure_numbers:
+                return True
+            # case 2 (valid): the input was 0
+            elif number_chosen == 0:
+                return True
+            # case 3 (invalid): the input was an int out of bounds
+            else:
+                print(f'Your input: {number_chosen}, does not correspond to a measure, please try again')
+                return False
+        # else (invalid): the input was not an int
+        else:
+            print(f'Your input: {user_input}, is not valid, please enter an integer')
+            return False
+
     print('Choose one of the following measures:')
     for measure in measures:
         measure.menu()
@@ -25,43 +52,17 @@ def choose_measure():
         if validate_measure_input(user_input):
             number_chosen = int(user_input)
             if number_chosen != 0:
-                measure_chosen= measures[int(number_chosen) - 1]
+                measure_chosen = measures[int(number_chosen) - 1]
                 if measure_chosen.is_active() is False:
-                    measure_chosen.activate()
                     print(f'You chose: {measure_chosen.name}, it will be activated')
                     return measure_chosen
                 elif measure_chosen.is_active() is True:
-                    return None  # TODO: handle undoing measures correctly
+                    print(f'You chose: {measure_chosen.name}, it will be deactivated')
+                    return measure_chosen
             elif number_chosen == 0:
                 print('You decided to take no action, the game will move on')
                 return None
 
-
-def validate_measure_input(user_input):
-    """Returns a boolean whether the user's input to choose a measure is valid"""
-    is_int = True
-    number_chosen = -1
-    try:
-        number_chosen = int(user_input)
-    except ValueError:
-        is_int = False
-
-    # if: the input was an int
-    if is_int:
-        # case 1 (valid): the input was an int corresponding to a measure
-        if number_chosen in measure_numbers:
-            return True
-        # case 2 (valid): the input was 0
-        elif number_chosen == 0:
-            return True
-        # case 3 (invalid): the input was an int out of bounds
-        else:
-            print(f'Your input: {number_chosen}, does not correspond to a measure, please try again')
-            return False
-    # else (invalid): the input was not an int
-    else:
-        print(f'Your input: {user_input}, is not valid, please enter an integer')
-        return False
 
 # create general setup
 measures, measure_numbers = initialise_measures()
@@ -87,10 +88,14 @@ while running:
     display_report()
     print(starline)
 
-    # choose a measure and get the corresponding factor
+    # choose a measure, (de)activate it, and get the corresponding factor
     new_measure = choose_measure()
-    if isinstance(new_measure, Measure):
+    if isinstance(new_measure, Measure) and new_measure.is_active() is False:
+        new_measure.activate()
         effect = new_measure.factor
+    elif isinstance(new_measure, Measure) and new_measure.is_active() is True:
+        new_measure.deactivate()
+        effect = 1 / new_measure.factor
     else:
         effect = 1
 
