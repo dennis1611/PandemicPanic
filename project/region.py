37bc7e2316b441c28013d30fb81026cdf36ab543
Willem <W.S.Wolswijk@student.tudelft.nl>
2020-09-30 17:17:16 +0200

    Limit to infections based of amount of recoveries

--- project/region.py
+++ project/region.py
@@ -10,16 +10,18 @@ class Region:
     Class that contains all information of a certain region.
     """
 
-    def __init__(self, name, inhabitants, infectionfactor, deathfactor, base_r=3, base_inf=1000):
+    def __init__(self, name, inhabitants, inffactor, dfactor, base_r=3, base_inf=1000, base_death_factor=0.02):
         self.name = name
         self.inhabitants = inhabitants
-        self.infectionfactor = infectionfactor
-        self.deathfactor = deathfactor
-        region_r = base_r * infectionfactor
+        self.infectionfactor = inffactor
+        self.deathfactor = dfactor
+        self.base_death_factor = base_death_factor
+
+        region_r = base_r * inffactor
 
         df = pd.DataFrame(data=[[base_inf, base_inf, 0, 0, 0, 0, region_r]],
-                          columns=['New infections', 'Total infections', 'New deaths', 'Total deaths', 'New recoveries',
-                                   'Total recoveries', 'R value'],
+                          columns=['New infections', 'Currently infected', 'New deaths', 'Total deaths',
+                                   'New recoveries', 'Total recoveries', 'R value'],
                           index=[0])
         self.df = df
 
@@ -34,22 +36,27 @@ class Region:
         # Assumption is made that people stay sick for two weeks
 
         prev_data = self.df.loc[current_week - 1]
-        prev_inf_total = prev_data.loc['Total infections']
+        prev_inf_total = prev_data.loc['Currently infected']
         prev_r = prev_data.loc['R value']
 
         new_infections = (1 / 2) * prev_r * prev_inf_total // 1
 
+        if new_infections > (self.inhabitants - prev_data.loc['Total recoveries']):
+            new_infections = self.inhabitants - prev_data.loc['Total recoveries']
+
         if current_week >= 2:
             prev_prev_inf_new = self.df.loc[current_week - 2, 'New infections']
-            new_deaths = (prev_prev_inf_new * self.deathfactor) // 1
+            new_deaths = (prev_prev_inf_new * self.deathfactor * self.base_death_factor) // 1
             new_recovs = prev_prev_inf_new - new_deaths
         else:
             new_deaths = 0
             new_recovs = 0
 
+        cur_infectious = prev_data.loc['Currently infected'] + new_infections - new_recovs - new_deaths
+
         new_data = {'New infections': new_infections,
-                    'Total infections': prev_data.loc['Total infections'] + new_infections - new_recovs - new_deaths,
-                    'New deaths': new_deaths, 'Total deaths': new_deaths + prev_data.loc['Total deaths'], 'R value': None,
+                    'Currently infected': cur_infectious, 'New deaths': new_deaths,
+                    'Total deaths': new_deaths + prev_data.loc['Total deaths'], 'R value': None,
                     'New recoveries': new_recovs, 'Total recoveries': new_recovs + prev_data.loc['Total recoveries']}
         self.df = self.df.append(new_data, ignore_index=True)
 
