5ce2c14cad2451f85407e79835a4e4b675b87ce7
Dennis <dennis16112000r@gmail.com>
2020-10-17 15:59:47 +0200

    make test balancing-insensitive

--- project/test/test_region.py
+++ project/test/test_region.py
@@ -22,7 +22,7 @@ class MyTestCase(unittest.TestCase):
         name = "Groningen"
         inhabitants = 585866
         self.assertEqual((self.regions[0].name, self.regions[0].inhabitants), (name, inhabitants))
-        self.assertAlmostEqual(self.regions[0].death_factor, 0.01984)
+        self.assertAlmostEqual(self.regions[0].death_factor, 0.992*0.02)
 
     def test_calculations_no_measures(self):
         name = "TestRegion"
