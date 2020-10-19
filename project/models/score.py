e4cd8dcebda0f2c069e68ae62b92c35ef12ae1c3
Jelle Knibbe <J.J.Knibbe@student.tudelft.nl>
2020-10-19 18:00:49 +0200

    35 added Score class, added first placeholder functions

--- project/models/score.py
+++ project/models/score.py
@@ -4,15 +4,18 @@ class Score:
 
     @staticmethod
     def base_penalty(measure):
-        # determine the base pnealty for a certain measure
+        # determine the base penalty for a certain measure
         return 1 - measure.factor
 
-    def penalize_measure(self, week, region, measures):
+    def penalize_measure(self, region, measures):
         # adjust the score based on measure taken and number of infections
+        # TODO: figure out how to make this function read the measures currently active in a region
         limit = 0.05
         light = 1
         heavy = 3
-        if region.df["Currently Infected"] > limit * region.inhabitants
-            pass
-        else:
-            pass
+        for measure in measures:
+            if measure.active:
+                if region.df["Currently Infected"] > limit * region.inhabitants:
+                    self.score -= self.base_penalty(measure) * heavy
+                else:
+                    self.score -= self.base_penalty(measure) * light
