3c3bc589f2e4fc121707f01e50f5fc80297412c9
Willem <W.S.Wolswijk@student.tudelft.nl>
2020-10-30 16:18:29 +0100

    Improved graphs.py

--- project/views/graphs.py
+++ project/views/graphs.py
@@ -12,29 +12,34 @@ def plot_graphs(regions):
     """
 
     # which columns from region.df to plot
-    to_plot_cols = ["Currently infected", "Total deaths"]
-    num = 1
     plt.figure(figsize=(18, 6))
-    for to_plot in to_plot_cols:
-        plt.subplot(1, 3, num)
-        for region in regions:
-            region.df[to_plot].plot()
-        plt.legend([region.abbreviation for region in regions], loc=2)
-        plt.title(to_plot)
+    # also plot graph with infections per 100k people
+
+    cols_to_plot = ['New infections', 'Currently infected', 'Total deaths']
+    titles = ['Newly infected per 100k inhabitants', 'Infected per 100k inhabitants', 'Total deaths']
+    for num, col in enumerate(cols_to_plot):
+        plt.subplot(1, 3, num + 1)
+
+        if col == "Currently infected":
+            plt.axhline(100, label="measures too strict", c="g", ls="--", )
+            plt.axhline(1000, label="hospitals are full", c="r", ls="--")
+            for region in regions:
+                plt.plot(region.df[col].tolist() / region.inhabitants * 100000, label=region.abbreviation)
+
+        elif col == "New infections":
+            for region in regions:
+                plt.plot(region.df[col].tolist() / region.inhabitants * 100000, label=region.abbreviation)
+
+        elif col == "Total deaths":
+            for region in regions:
+                region.df["Total deaths"].plot()
+
+        plt.title(titles[num])
         plt.xlabel("Week")
         plt.ylabel("# of people")
-        plt.ticklabel_format(axis="y", style="plain", scilimits=(0, 0))
+        plt.legend(loc=2)
         plt.grid()
-        num += 1
-    # also plot graph with infections per 100k people
-    plt.subplot(1, 3, 3)
-    for region in regions:
-        plt.plot(region.df["Currently infected"].tolist() / region.inhabitants * 100000, label=region.abbreviation)
-    plt.title("Infected per 100k")
-    plt.xlabel("Week")
-    plt.ylabel("# of people")
-    plt.axhline(100, label="measures too strict", c="g", ls="--", )
-    plt.axhline(1000, label="hospitals are full", c="r", ls="--")
-    plt.legend(loc=2)
-    plt.grid()
+        plt.ticklabel_format(axis="y", style="plain", scilimits=(0, 0))
+        plt.legend([region.abbreviation for region in regions], loc=2)
+
     plt.show()
